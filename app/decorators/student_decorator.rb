e3c8169b8ff74dc2b90243f2b03906ea90d6e8e5
Tomasz Jaśkiewicz <jasek2@gmail.com>
2015-08-23 17:59:14 +0200

    decorators

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -4,5 +4,11 @@ class StudentDecorator < BaseDecorator
   end
 
   def avg_notes(subject_item)
+    avg = 0
+    arr = subject_item_notes.select{|a| a.subject_item == subject_item}.map{|b| b.value}
+    if (!arr.empty?)
+      avg = arr.inject{ |sum, el| sum + el }.to_f / arr.size
+    end
+    "#{'%.2f' % avg}"
   end
 end
