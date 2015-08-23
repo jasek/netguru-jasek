be7829ebc928c7ab7306b0fa34f1b45998d68fd4
Tomasz Jaśkiewicz <jasek2@gmail.com>
2015-08-23 23:12:23 +0200

    add birthday column

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -5,10 +5,17 @@ class StudentDecorator < BaseDecorator
 
   def avg_notes(subject_item)
     avg = 0
-    arr = subject_item_notes.select{|a| a.subject_item == subject_item}.map{|b| b.value}
+    arr = subject_item_notes.select { |a| a.subject_item == subject_item }.map { |b| b.value }
     if (!arr.empty?)
-      avg = arr.inject{ |sum, el| sum + el }.to_f / arr.size
+      avg = arr.inject { |sum, el| sum + el }.to_f / arr.size
     end
     "#{'%.2f' % avg}"
   end
+
+  def birthday_decorator
+    if !birthday.nil?
+      "#{birthday.year}-#{format('%02d', birthday.month)}_#{format('%02d', birthday.day)}"
+    end
+  end
 end
+
